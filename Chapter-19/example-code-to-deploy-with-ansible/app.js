6c0dd833acc4c5d28e9c9a3b4c8a4d1b7e21255b
Nikhil Pathania <nikhilpathania@hotmail.com>
2019-04-22 20:42:36 +0200

    added source code

---
+++ Chapter-19/example-code-to-deploy-with-ansible/app.js
@@ -0,0 +1,33 @@
+/*
+ * Module dependencies
+ */
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+var logger = require('morgan')
+
+var app = express()
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'pug')
+app.use(logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
+})
+
+app.listen(3000)
